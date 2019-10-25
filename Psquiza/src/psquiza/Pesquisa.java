7217e37a1c9d987f9a2c32cb56be18412f49de84
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2019-10-25 07:59:28 -0300

    teste de envio case 1

--- Psquiza/src/psquiza/Pesquisa.java
+++ Psquiza/src/psquiza/Pesquisa.java
@@ -1,5 +1,18 @@
 package psquiza;
 
 public class Pesquisa {
-
+	
+	private String descricao;
+	private String campoDeInteresse;
+	private String codigo;
+	public Pesquisa(String descricao, String campoDeInteresse, String codigo) {
+		super();
+		this.descricao = descricao;
+		this.campoDeInteresse = campoDeInteresse;
+		this.codigo = codigo;
+	}
+	
+	
+	
+	
 }
---
+++ Psquiza/src/psquiza/PesquisaController.java
@@ -0,0 +1,70 @@
+	package psquiza;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+	* Laboratório de Programação 2 - Lab 1
+	* 
+	* @author Matheus Vinicius Benevides Lima - 119210384
+	*/
+	public class PesquisaController {
+
+		private Map<String, Pesquisa> mapPesquisas = new HashMap<String, Pesquisa>();
+
+		public String cadastraPesquisa(String descricao, String campoDeInteresse) {
+
+			String codigo = "";
+			char[] charPesquisas = new char[255];
+			String[] arrayCodigo = campoDeInteresse.split(", ");
+			int numero = 0;
+			for (String nome : arrayCodigo) {
+				for (int i = 0; i < 3;) {
+					charPesquisas[0] = nome.charAt(0);
+					charPesquisas[1] = nome.charAt(1);
+					charPesquisas[2] = nome.charAt(2);
+
+					break;
+
+				}
+				break;
+
+			}
+
+			for (int i = 0; i < 3; i++) {
+				if (i < 3) {
+					codigo += charPesquisas[i];
+				} else {
+					codigo += numero;
+					numero++;
+				}
+
+			}
+			
+			mapPesquisas.put(codigo, new Pesquisa(descricao, campoDeInteresse, codigo));
+
+			return codigo;
+
+		}
+
+		public void alteraPesquisa(String código, String conteúdoASerAlterado, String novoConteúdo) {
+
+		}
+
+		public void encerraPesquisa(String codigo, String motivo) {
+
+		}
+
+		public void ativaPesquisa(String codigo) {
+
+		}
+
+		public String exibePesquisa(String codigo) {
+			return "";
+		}
+
+		public boolean ehAtiva(String codigo) {
+			return true;
+		}
+
+	}
