99083863a533d4ea52163f0c4ed1ed0f16dbb98b
WeslleyS <weslley.azevedo@ccc.ufcg.edu.br>
2019-10-31 11:16:44 -0300

    Atualizei a facade.

--- Psquiza/src/psquiza/Facade.java
+++ Psquiza/src/psquiza/Facade.java
@@ -25,7 +25,7 @@ public class Facade {
 
 	}
 
-	// METODOS DA PARTE 1 (MATHEUS) ABAIXO DESSE COMENTARIO
+	// METODOS DA PARTE 1 (WESLLEY) ABAIXO DESSE COMENTARIO
 	public String cadastraPesquisa(String descricao, String campoDeInteresse) {
 		return pesquisaController.cadastraPesquisa(descricao, campoDeInteresse);
 	}
@@ -50,70 +50,28 @@ public class Facade {
 		return pesquisaController.ehAtiva(codigo);
 	}
 
-	// METODOS DA PARTE 2 (RAPHAEL) ABAIXO DESSE COMENTARIO
+	// METODOS DA PARTE 2 (MATHEUS) ABAIXO DESSE COMENTARIO
 
-	/**
-	 * Metodo que cadastra um pesquisador a partir do seu nome, funcao, biografia,
-	 * email e fotoURL.
-	 * 
-	 * @param nome      Nome do pesquisador.
-	 * @param funcao    Funcao do pesquisador.
-	 * @param biografia Biografia do pesquisador.
-	 * @param email     Email do pesquisador.
-	 * @param fotoURL   Url da foto do pesquisador.
-	 */
 	public void cadastraPesquisador(String nome, String funcao, String biografia, String email, String fotoURL) {
 		this.pesquisadorController.cadastraPesquisador(nome, funcao, biografia, email, fotoURL);
 	}
 
-	/**
-	 * Metodo que edita um pesquisador a partir do seu email, do atributo a ser
-	 * editado e um novo valor para o atributo.
-	 * 
-	 * @param email     Email do pesquisador.
-	 * @param atributo  Atributo que vai ser editado.
-	 * @param novoValor Novo valor para o atributo.
-	 */
 	public void alteraPesquisador(String email, String atributo, String novoValor) {
 		this.pesquisadorController.alteraPesquisador(email, atributo, novoValor);
 	}
 
-	/**
-	 * Metodo responsavel por ativar o pesquisador a partir do seu email.
-	 * 
-	 * @param email Email do pesquisador
-	 */
 	public void ativaPesquisador(String email) {
 		this.pesquisadorController.ativaPesquisador(email);
 	}
 
-	/**
-	 * Metodo responsavel por desativar o pesquisador a partir do seu email.
-	 * 
-	 * @param email Email do pesquisador
-	 */
 	public void desativaPesquisador(String email) {
 		this.pesquisadorController.desativaPesquisador(email);
 	}
 
-	/**
-	 * Metodo que retorna a representacao em string do pesquisador a partir do seu
-	 * cpf.
-	 * 
-	 * @param email Email do pesquisador
-	 * @return retorna a representacao em string do pesquisador
-	 */
 	public String exibePesquisador(String email) {
 		return this.pesquisadorController.exibePesquisador(email);
 	}
 
-	/**
-	 * Metodo que verifica se um pesquisador é ativo ou nao, a partir do seu email.
-	 * 
-	 * @param email Email do pesquisador.
-	 * @return um valor booleano verdade caso o pesquisador esteja ativo, caso
-	 *         contrario retorna falso.
-	 */
 	public boolean pesquisadorEhAtivo(String email) {
 		return this.pesquisadorController.pesquisadorEhAtivo(email);
 	}
@@ -144,67 +102,28 @@ public class Facade {
 		return this.objetivoController.exibeObjetivo(codigo);
 	}
 
-	// METODOS DA PARTE 4 (WESLLEY) ABAIXO DESSE COMENTARIO
+	// METODOS DA PARTE 4 (RAPHAEL) ABAIXO DESSE COMENTARIO
 
-	/**
-	 * Metodo que cadastra uma atividade a partir da sua descricao, nivel de risco e
-	 * descricao do risco.
-	 * 
-	 * @param descricao      Descricao da atividade.
-	 * @param nivelRisco     Nivel de risco da atividade.
-	 * @param descricaoRisco Descricao do risco da atividade.
-	 * @return a representacao em String do codigo de identificacao da atividade.
-	 */
 	public String cadastraAtividade(String descricao, String nivelRisco, String descricaoRisco) {
 		return this.atividadeController.cadastraAtividade(descricao, nivelRisco, descricaoRisco);
 	}
 
-	/**
-	 * Apaga uma atividade a partir de seu codigo.
-	 * 
-	 * @param codigo Codigo da atividade.
-	 */
 	public void apagaAtividade(String codigo) {
 		this.atividadeController.apagaAtividade(codigo);
 	}
 
-	/**
-	 * Cadastra um item na atividade a partir do codigo da atividade e a String que
-	 * representa o item.
-	 * 
-	 * @param codigo Codigo da atividade.
-	 * @param item   Representacao em String do item.
-	 */
 	public void cadastraItem(String codigo, String item) {
 		this.atividadeController.cadastraItem(codigo, item);
 	}
 
-	/**
-	 * Retorna a String que representa a atividade.
-	 * 
-	 * @param codigo Codigo da atividade.
-	 * @return a representacao em String da atividade.
-	 */
 	public String exibeAtividade(String codigo) {
 		return this.atividadeController.exibeAtividade(codigo);
 	}
 
-	/**
-	 * Retorna a quantidade de itens pendentes contidos em determinada atividade.
-	 * 
-	 * @param codigo Codigo da atividade.
-	 * @return a representacao em Int da quantidade de itens pendentes.
-	 */
 	public int contaItensPendentes(String codigo) {
 		return this.atividadeController.contaItensPedentes(codigo);
 	}
 
-	/**
-	 * Retorna a quantidade de itens realizados contidos em determinada atividade.
-	 * 
-	 * @param codigo Codigo da atividade.
-	 * @return a representacao em Int da quantidade de itens realizados.
-	 */
 	public int contaItensRealizados(String codigo) {
 		return this.atividadeController.contaItensRealizados(codigo);
 	}
