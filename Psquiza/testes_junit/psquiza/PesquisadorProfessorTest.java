72ad14365af00e95afe557d6cf09ba027c000486
Weslley Azevedo <weslley.azevedo@ccc.ufcg.edu.br>
2019-11-10 21:10:02 -0300

    Atualizei os testes US5

---
+++ Psquiza/testes_junit/psquiza/PesquisadorProfessorTest.java
@@ -0,0 +1,39 @@
+package psquiza;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PesquisadorProfessorTest {
+
+	@Test
+	void testPesquisadorProfessor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFormacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUnidadeAlocada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDataContratacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAlteraPesquisador() {
+		fail("Not yet implemented");
+	}
+
+}
